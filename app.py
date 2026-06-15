6121af0e64e906310113b97bdfe2df8ae08ee612
mimozamaliqi111-code <mimozamaliqi.111@gmail.com>
2026-06-15 19:35:00 +0200

    dita 1

---
+++ app.py
@@ -0,0 +1,11 @@
+"""I NEED A BREAK - Flask app"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("game.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
